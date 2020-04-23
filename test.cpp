2bbd0ebd6a5de5768f2fe8490b77314dacdcdf6f
mwyrick <mantzwyr@gmail.com>
2020-04-22 18:37:06 -0700

    adding c++ and executable files

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+//using std::vector;
+
+
+int main(void)
+{
+	string string1 = "This is a test c++ file through gitBash", string2 = "can use vi as a text editor through gitBash";
+
+	cout << string1 << endl;
+	cout << string2 << endl;
+	return 0;
+}
+
